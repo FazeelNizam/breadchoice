cb8382ed285d33ac0ebea54269d833d378db1e8f
FazeelNizam <nizamfazeel@gmail.com>
2023-06-07 22:51:37 +0530

    june 7 2023 finished navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import Hamburger from 'hamburger-react'
+import { MdLogin, MdLogout, MdOutlineLibraryAdd } from 'react-icons/md'
+
+import { useStateValue } from '../context/StateProvider'
+import { actionType } from '../context/reducer'
+import { app } from '../firebase.config'
 
 import Logo from '../img/Logo.png'
 import Avatar from '../img/avatar.png'
@@ -10,6 +16,42 @@ import './main.css'
 
 const NavBar = () => {
   const [isOpen, setOpen] = useState(false)
+  const [isMenu, setIsMenu] = useState(false)
+
+  const firebaseAuth = getAuth(app)
+  const provider = new GoogleAuthProvider()
+
+  const [{ user }, dispatch] = useStateValue()
+
+  const login = async () => {
+    try {
+      if (!user) {
+        const {
+          user: { refreshToken, providerData },
+        } = await signInWithPopup(firebaseAuth, provider)
+        dispatch({
+          type: actionType.SET_USER,
+          user: providerData[0],
+        })
+        localStorage.setItem('user', JSON.stringify(providerData[0]))
+      } else {
+        setIsMenu(!isMenu)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const logout = () => {
+    setIsMenu(false)
+    setOpen(false)
+    localStorage.clear()
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    })
+  }
 
   return (
     <>
@@ -19,9 +61,9 @@ const NavBar = () => {
         exit={{ opacity: 0, y: -200 }}
       >
         <div className="navbar">
-          <motion.Link to={'/'} className="logo">
+          <Link to={'/'} className="logo">
             <img src={Logo} alt="logo" />
-          </motion.Link>
+          </Link>
           <div className="menu">
             <ul>
               <li>
@@ -41,8 +83,12 @@ const NavBar = () => {
               <motion.div className="cart-icon" whileTap={{ scale: 0.6 }}>
                 <img src={Cart} alt="cart" />
               </motion.div>
-              <motion.div className="user-icon" whileTap={{ scale: 0.6 }}>
-                <img src={Avatar} alt="avatar" />
+              <motion.div
+                className="user-icon"
+                whileTap={{ scale: 0.6 }}
+                onClick={login}
+              >
+                <img src={user ? user.photoURL : Avatar} alt="avatar" />
               </motion.div>
               <div className="hamberger">
                 <Hamburger toggled={isOpen} toggle={setOpen} />
@@ -51,13 +97,70 @@ const NavBar = () => {
           </div>
         </div>
       </motion.nav>
+
+      {/* user details menu */}
+
+      {isMenu && (
+        <motion.div
+          className="user-details-container"
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ y: -100 }}
+        >
+          <div className="user-details-menu">
+            <motion.div
+              className="user-icon"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ y: 20 }}
+              transition={{ delay: 0.2, type: 'tween' }}
+            >
+              <img src={user ? user.photoURL : Avatar} alt="avatar" />
+              <div className="u-details">
+                <span className="u-name">{user.displayName}</span>
+                <span className="u-mail">{user.email}</span>
+              </div>
+              <motion.div
+                className="logout"
+                whileTap={{ scale: 0.6 }}
+                onClick={logout}
+              >
+                <MdLogout />
+              </motion.div>
+            </motion.div>
+            <hr />
+
+            {user && user.email === 'nizamfazeel@gmail.com' && (
+              <>
+                <motion.div
+                  className="add-new-item"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ y: 20 }}
+                  transition={{ delay: 0.2, type: 'tween' }}
+                  onClick={() => setIsMenu(false)}
+                >
+                  <motion.div whileTap={{ scale: 0.9 }}>
+                    <Link to={'/createItem'}>
+                      New Item <MdOutlineLibraryAdd />
+                    </Link>
+                  </motion.div>
+                </motion.div>
+                <hr />
+              </>
+            )}
+          </div>
+          <span className="overlay"></span>
+        </motion.div>
+      )}
+
       {/* mobile */}
       {isOpen && (
         <motion.div
           className="mobile-menu"
           initial={{ opacity: 0, x: 400 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 400 }}
+          exit={{ x: 400 }}
         >
           <div className="mobile-menu-list">
             <ul>
@@ -66,21 +169,70 @@ const NavBar = () => {
                   className="user-icon-container"
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 20 }}
+                  exit={{ y: 20 }}
                   transition={{ delay: 0.2, type: 'tween' }}
                 >
-                  <motion.div className="user-icon" whileTap={{ scale: 0.6 }}>
-                    <img src={Avatar} alt="avatar" />
-                  </motion.div>
-                  <p>Hello, Fazeel</p>
+                  {user ? (
+                    <>
+                      <motion.div
+                        className="user-icon"
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ y: 20 }}
+                        transition={{ delay: 0.2, type: 'tween' }}
+                      >
+                        <img src={user ? user.photoURL : Avatar} alt="avatar" />
+                        <div className="u-details">
+                          <span className="u-name">{user.displayName}</span>
+                          <span className="u-mail">{user.email}</span>
+                        </div>
+                        <motion.div
+                          className="logout"
+                          whileTap={{ scale: 0.6 }}
+                          onClick={logout}
+                        >
+                          <MdLogout />
+                        </motion.div>
+                      </motion.div>
+                    </>
+                  ) : (
+                    <>
+                      <motion.div
+                        className="menu-login"
+                        onClick={login}
+                        whileTap={{ scale: 0.8 }}
+                      >
+                        Login <MdLogin />
+                      </motion.div>
+                    </>
+                  )}
                 </motion.div>
               </li>
               <hr />
+
+              {user && user.email === 'nizamfazeel@gmail.com' && (
+                <>
+                  <motion.li
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ y: 20 }}
+                    transition={{ delay: 0.2, type: 'tween' }}
+                    onClick={() => setOpen(false)}
+                  >
+                    <Link to={'/createItem'}>
+                      New Item <MdOutlineLibraryAdd />
+                    </Link>
+                  </motion.li>
+                  <hr />
+                </>
+              )}
+
               <motion.li
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 20 }}
+                exit={{ y: 20 }}
                 transition={{ delay: 0.2, type: 'tween' }}
+                onClick={() => setOpen(false)}
               >
                 <Link to={'/'}>Home</Link>
               </motion.li>
@@ -88,8 +240,9 @@ const NavBar = () => {
               <motion.li
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 20 }}
+                exit={{ y: 20 }}
                 transition={{ delay: 0.2, type: 'tween' }}
+                onClick={() => setOpen(false)}
               >
                 <a href="#menu">Menu</a>
               </motion.li>
@@ -97,8 +250,9 @@ const NavBar = () => {
               <motion.li
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 20 }}
+                exit={{ y: 20 }}
                 transition={{ delay: 0.2, type: 'tween' }}
+                onClick={() => setOpen(false)}
               >
                 <a href="#reviews">Reviews</a>
               </motion.li>
@@ -106,23 +260,16 @@ const NavBar = () => {
               <motion.li
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 20 }}
+                exit={{ y: 20 }}
                 transition={{ delay: 0.2, type: 'tween' }}
+                onClick={() => setOpen(false)}
               >
                 <a href="#q&a">Q & A</a>
               </motion.li>
               <hr />
-              <motion.li
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 20 }}
-                transition={{ delay: 0.2, type: 'tween' }}
-              >
-                <a href="#">Log Out</a>
-              </motion.li>
-              <hr />
             </ul>
           </div>
+          <span className="overlay"></span>
         </motion.div>
       )}
     </>
